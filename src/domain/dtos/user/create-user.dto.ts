c764c6ded65a70140ee1261e9c0d30151fc8d7bd
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-27 13:25:36 -0500

    refactor CreateUserDto to consolidate validation checks for user properties

--- src/domain/dtos/user/create-user.dto.ts
+++ src/domain/dtos/user/create-user.dto.ts
@@ -8,9 +8,7 @@ export class CreateUserDto {
   static create(props: { [key: string]: any }): [string?, CreateUserDto?] {
     const { name, lastname, authId } = props;
 
-    if (!name) return ["invalid Data"];
-    if (!lastname) return ["invalid Data"];
-    if (!authId) return ["invalid Data"];
+    if (!name || !lastname || !authId) return ["invalid Data"];
 
     return [, new CreateUserDto(name, lastname, authId)];
   }
