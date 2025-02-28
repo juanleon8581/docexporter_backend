75d0da5774a414d3d222c5d438386cbccb147330
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-28 17:43:59 -0500

    fix: correct property name from lastName to lastname in CreateUserDto

--- src/domain/dtos/user/create-user.dto.ts
+++ src/domain/dtos/user/create-user.dto.ts
@@ -1,7 +1,7 @@
 export class CreateUserDto {
   constructor(
     public readonly name: string,
-    public readonly lastName: string,
+    public readonly lastname: string,
     public readonly authId: string
   ) {}
 
