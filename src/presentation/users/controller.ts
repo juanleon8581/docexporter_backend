a0a920c82e112d2283808350c7722a655dfc7dc8
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-28 18:42:06 -0500

    feat: implement UsersController with CRUD operations for user management

--- src/presentation/users/controller.ts
+++ src/presentation/users/controller.ts
@@ -1,17 +1,58 @@
-export class UsersController {
-  public getUsers = (req: Request, res: Response) => {
-    throw new Error("Method Not implemented");
-  };
-  public getUserById = (req: Request, res: Response) => {
-    throw new Error("Method Not implemented");
-  };
+import { Request, Response } from "express";
+import { UserRepository } from "@/domain/repositories";
+import { CreateUserDto, UpdateUserDto } from "@/domain/dtos";
+import {
+  CreateUser,
+  DeleteUser,
+  GetUser,
+  GetUsers,
+  UpdateUser,
+} from "@/domain/use-cases";
+
+export class UserController {
+  constructor(private readonly userRepository: UserRepository) {}
+
   public createUser = (req: Request, res: Response) => {
-    throw new Error("Method Not implemented");
+    const [error, createUserDto] = CreateUserDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    new CreateUser(this.userRepository)
+      .execute(createUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ error }));
   };
+
+  public getUsers = (req: Request, res: Response) => {
+    new GetUsers(this.userRepository)
+      .execute()
+      .then((users) => res.json(users))
+      .catch((error) => res.status(400).json({ error }));
+  };
+
+  public getUserById = (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    new GetUser(this.userRepository)
+      .execute(id)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ error }));
+  };
+
   public updateUser = (req: Request, res: Response) => {
-    throw new Error("Method Not implemented");
+    const [error, updateUserDto] = UpdateUserDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    new UpdateUser(this.userRepository)
+      .execute(updateUserDto!)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ error }));
   };
   public deleteUser = (req: Request, res: Response) => {
-    throw new Error("Method Not implemented");
+    const { id } = req.params;
+
+    new DeleteUser(this.userRepository)
+      .execute(id)
+      .then((user) => res.json(user))
+      .catch((error) => res.status(400).json({ error }));
   };
 }
