d1a55e83df97ca34c372e1bd0ffeb97e2ef9d4b6
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-28 19:56:14 -0500

    feat: implement user CRUD operations in UsersRoutes

--- src/presentation/users/routes.ts
+++ src/presentation/users/routes.ts
@@ -1,14 +1,29 @@
 import { Request, Response, Router } from "express";
+import { UserController } from "@/presentation/users/controller";
+import { UserDatasourceImpl } from "@/infrastructure/tadasource/user.datasource.impl";
+import { UserRepositoryImpl } from "@/infrastructure/repositories/user.repository.impl";
 
 export class UsersRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get("/", (req: Request, res: Response) => {
-      res.json({
-        data: "implementing...",
-      });
+    const userDatasource = new UserDatasourceImpl();
+    const userRepository = new UserRepositoryImpl(userDatasource);
+    const userController = new UserController(userRepository);
+
+    router.post("/", (req: Request, res: Response) => {
+      userController.createUser(req, res);
     });
+
+    router.get("/", userController.getUsers);
+    router.get("/:id", userController.getUserById);
+
+    router.put("/", (req: Request, res: Response) => {
+      userController.updateUser(req, res);
+    });
+
+    router.delete("/:id", userController.deleteUser);
+
     return router;
   }
 }
