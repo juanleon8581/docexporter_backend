7f85c467aee13fcc73136473ae99375fec428bfb
Juan Pablo Leon <juanleon8581@gmail.com>
2025-03-01 18:55:12 -0500

    feat: add PayOrderTemplate routes for CRUD operations

---
+++ src/presentation/pay-order-template/routes.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { Router } from "express";
+import { PayOrderTemplateController } from "./controller";
+import { PayOrderTemplateRepositoryImpl } from "@/infrastructure/repositories/pay-order-template.repository.impl";
+import { PayOrderTemplateDatasourceImpl } from "@/infrastructure/datasource/pay-order-template.datasource.impl";
+
+export class PayOrderTemplateRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const dataSource = new PayOrderTemplateDatasourceImpl();
+    const repository = new PayOrderTemplateRepositoryImpl(dataSource);
+    const controller = new PayOrderTemplateController(repository);
+
+    router.post("/", (req: Request, res: Response) => {
+      controller.createPayOrderTemplate(req, res);
+    });
+
+    router.get("/", controller.getPayOrderTemplate);
+    router.get("/:id", controller.getPayOrderTemplates);
+
+    router.put("/", (req: Request, res: Response) => {
+      controller.updatePayOrderTemplate(req, res);
+    });
+
+    router.delete("/:id", controller.deletePayOrderTemplate);
+
+    return router;
+  }
+}
