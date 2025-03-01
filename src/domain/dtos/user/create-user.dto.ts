ecf28764761a329da99bb763bd77dafa99488b0f
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-28 19:17:18 -0500

    fix: correct property name from authId to authid in CreateUserDto

--- src/domain/dtos/user/create-user.dto.ts
+++ src/domain/dtos/user/create-user.dto.ts
@@ -2,14 +2,14 @@ export class CreateUserDto {
   constructor(
     public readonly name: string,
     public readonly lastname: string,
-    public readonly authId: string
+    public readonly authid: string
   ) {}
 
   static create(props: { [key: string]: any }): [string?, CreateUserDto?] {
-    const { name, lastname, authId } = props;
+    const { name, lastname, authid } = props;
 
-    if (!name || !lastname || !authId) return ["invalid Data"];
+    if (!name || !lastname || !authid) return ["invalid Data"];
 
-    return [, new CreateUserDto(name, lastname, authId)];
+    return [, new CreateUserDto(name, lastname, authid)];
   }
 }
