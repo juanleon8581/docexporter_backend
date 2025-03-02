8eb2f7d6e38b95aeda4713c69c2a8faee480c130
Juan Pablo Leon <juanleon8581@gmail.com>
2025-03-01 19:00:29 -0500

    feat: add PayOrderTemplate routes to main application routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { UsersRoutes } from "./users/routes";
+import { PayOrderTemplateRoutes } from "./pay-order-template/routes";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use("/api/users", UsersRoutes.routes);
+    router.use("/api/payorder", PayOrderTemplateRoutes.routes);
 
     return router;
   }
