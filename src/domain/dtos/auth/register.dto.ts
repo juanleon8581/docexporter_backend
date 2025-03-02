160e85aa72eddd485d4ab570b9304b2805e7e360
jpleon <juanleon8581@gmail.com>
2025-03-02 09:45:30 -0500

    fix: remove nickName from RegisterDto and validation in create method

--- src/domain/dtos/auth/register.dto.ts
+++ src/domain/dtos/auth/register.dto.ts
@@ -1,15 +1,14 @@
 export class RegisterDto {
   constructor(
     public readonly email: string,
-    public readonly password: string,
-    public readonly nickName: string
+    public readonly password: string
   ) {}
 
   static create(props: { [key: string]: any }): [string?, RegisterDto?] {
-    const { email, password, nickName } = props;
+    const { email, password } = props;
 
-    if (!email || !password || !nickName) return ["Invalid Data"];
+    if (!email || !password) return ["Invalid Data"];
 
-    return [, new RegisterDto(email, password, nickName)];
+    return [, new RegisterDto(email, password)];
   }
 }
