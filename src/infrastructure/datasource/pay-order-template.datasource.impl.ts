fb9afda56a92713e08942f284a8b2c9b77c1e2e2
Juan Pablo Leon <juanleon8581@gmail.com>
2025-03-03 22:35:45 -0500

    feat: Implement encryption and decryption for sensitive data in PayOrderTemplateDatasource

--- src/infrastructure/datasource/pay-order-template.datasource.impl.ts
+++ src/infrastructure/datasource/pay-order-template.datasource.impl.ts
@@ -6,18 +6,46 @@ import {
 } from "@/domain/dtos";
 import { PayOrderTemplateEntity } from "@/domain/entities";
 import { NotFoundError } from "@/errors/not-found-error";
+import { CryptoAdapter } from "@/config/adapters/crypto.adapter";
+
+type EncryptDataDto = CreatePayOrderTemplateDto | UpdatePayOrderTemplateDto;
 
 export class PayOrderTemplateDatasourceImpl
   implements PayOrderTemplateDatasource
 {
+  private encryptSensitiveData(dto: EncryptDataDto): EncryptDataDto {
+    const encryptedDto = { ...dto };
+    if (dto.dni) encryptedDto.dni = CryptoAdapter.encrypt(dto.dni);
+    if (dto.bank) encryptedDto.bank = CryptoAdapter.encrypt(dto.bank);
+    if (dto.accountNumber)
+      encryptedDto.accountNumber = CryptoAdapter.encrypt(dto.accountNumber);
+    return encryptedDto;
+  }
+
+  private decryptSensitiveData(template: {
+    [key: string]: any;
+  }): PayOrderTemplateEntity {
+    const decryptedTemplate = { ...template };
+    if (template.dni)
+      decryptedTemplate.dni = CryptoAdapter.decrypt(template.dni);
+    if (template.bank)
+      decryptedTemplate.bank = CryptoAdapter.decrypt(template.bank);
+    if (template.accountNumber)
+      decryptedTemplate.accountNumber = CryptoAdapter.decrypt(
+        template.accountNumber
+      );
+    return PayOrderTemplateEntity.fromJson(decryptedTemplate);
+  }
+
   async create(
     dto: CreatePayOrderTemplateDto
   ): Promise<PayOrderTemplateEntity> {
+    const encryptedDto = this.encryptSensitiveData(dto);
     const newPayOrderTemplate = await prisma.payOrderTemplate.create({
-      data: dto,
+      data: encryptedDto,
     });
 
-    return PayOrderTemplateEntity.fromJson(newPayOrderTemplate);
+    return this.decryptSensitiveData(newPayOrderTemplate);
   }
 
   async getAll(): Promise<PayOrderTemplateEntity[]> {
